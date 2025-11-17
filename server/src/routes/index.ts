0d3b541b5ad7bf13dc97ef74ec0b47046db17eed
Paweł Kędra <pawel.kedra@intive.com>
2025-11-17 21:45:16 +0100

    [002] Log statement

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,11 +3,13 @@ import express, { type Request, type Response } from 'express'
 const router = express.Router();
 
 router.get('/pets', async (_req: Request, res: Response) => {
-  res.json([
+  const output = [
     { name: 'Fafik' },
     { name: 'Reksio' },
     { name: 'Burek' },
-  ])
+  ]
+  console.log(`Output size: ${output.length}`)
+  res.json(output)
 })
 
 export default router;
