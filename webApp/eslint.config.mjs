3154f517dc8edd3c27a1790002e88efb5de5fafb
Paweł Kędra <pawel.kedra@intive.com>
2025-11-18 16:54:02 +0100

    [002] ESLint config for webapp

---
+++ webApp/eslint.config.mjs
@@ -0,0 +1,15 @@
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import reactPlugin from 'eslint-plugin-react';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+  {
+    ignores: ['node_modules/', 'dist/', 'build/', 'coverage/', '*.js'],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat['jsx-runtime']
+);
