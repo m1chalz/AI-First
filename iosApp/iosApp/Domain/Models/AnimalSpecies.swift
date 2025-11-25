7133512c9d092bd0d8c56d32206a39981c1575cc
Maciej Szmyt <maciej.szmyt@intive.com>
2025-11-25 18:27:44 +0100

    Add Codable conformance to AnimalSpecies enum
    - Implement custom Codable encoding/decoding for AnimalSpecies
    - Decode from uppercase string values (DOG, CAT, BIRD, RABBIT, OTHER)
    - Encode to uppercase string values
    - Default to .other for unknown values

--- iosApp/iosApp/Domain/Models/AnimalSpecies.swift
+++ iosApp/iosApp/Domain/Models/AnimalSpecies.swift
@@ -1,11 +1,51 @@
 import Foundation
 
 /// Animal species types supported by the system.
-enum AnimalSpecies {
+enum AnimalSpecies: Codable {
     case dog
     case cat
     case bird
     case rabbit
     case other
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let stringValue = try container.decode(String.self).uppercased()
+        
+        switch stringValue {
+        case "DOG":
+            self = .dog
+        case "CAT":
+            self = .cat
+        case "BIRD":
+            self = .bird
+        case "RABBIT":
+            self = .rabbit
+        case "OTHER":
+            self = .other
+        default:
+            self = .other
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        let stringValue: String
+        
+        switch self {
+        case .dog:
+            stringValue = "DOG"
+        case .cat:
+            stringValue = "CAT"
+        case .bird:
+            stringValue = "BIRD"
+        case .rabbit:
+            stringValue = "RABBIT"
+        case .other:
+            stringValue = "OTHER"
+        }
+        
+        try container.encode(stringValue)
+    }
 }
 
