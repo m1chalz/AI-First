ceea7dafd79bee84f2bb10cbdc3a4c785507115c
Maciej Szmyt <maciej.szmyt@intive.com>
2025-11-28 23:09:20 +0100

    Fix ReportMissingPetFlowStateTests after Animal Description refactoring
    Updated tests to reflect new granular properties in ReportMissingPetFlowState.
    Property 'description' was replaced with specific fields for Step 3:
    - disappearanceDate, animalSpecies, animalRace, animalGender
    - animalAge, animalLatitude, animalLongitude
    - animalAdditionalDescription
    
    Changes:
    - Updated testInit to check all new properties are nil
    - Updated testClear_whenPropertiesHaveValues with sample values for all fields
    - Updated testClear_whenPropertiesAlreadyNil to assert all new properties

--- iosApp/iosAppTests/Features/ReportMissingPet/Models/ReportMissingPetFlowStateTests.swift
+++ iosApp/iosAppTests/Features/ReportMissingPet/Models/ReportMissingPetFlowStateTests.swift
@@ -27,9 +27,21 @@ final class ReportMissingPetFlowStateTests: XCTestCase {
         
         // Then: All properties should be nil
         XCTAssertNil(sut.chipNumber)
+<<<<<<< HEAD
         XCTAssertNil(sut.photoAttachment)
         XCTAssertEqual(sut.photoStatus, .empty)
         XCTAssertNil(sut.description)
+=======
+        XCTAssertNil(sut.photo)
+        XCTAssertNil(sut.disappearanceDate)
+        XCTAssertNil(sut.animalSpecies)
+        XCTAssertNil(sut.animalRace)
+        XCTAssertNil(sut.animalGender)
+        XCTAssertNil(sut.animalAge)
+        XCTAssertNil(sut.animalLatitude)
+        XCTAssertNil(sut.animalLongitude)
+        XCTAssertNil(sut.animalAdditionalDescription)
+>>>>>>> d4eaf68 (Fix ReportMissingPetFlowStateTests after Animal Description refactoring)
         XCTAssertNil(sut.contactEmail)
         XCTAssertNil(sut.contactPhone)
     }
@@ -39,6 +51,7 @@ final class ReportMissingPetFlowStateTests: XCTestCase {
     func testClear_whenPropertiesHaveValues_shouldResetAllToNil() async {
         // Given: Flow state with populated properties
         sut.chipNumber = "123456789012345"
+<<<<<<< HEAD
         sut.photoAttachment = PhotoAttachmentMetadata(
             id: UUID(),
             fileName: "pet.jpg",
@@ -52,6 +65,17 @@ final class ReportMissingPetFlowStateTests: XCTestCase {
         )
         sut.photoStatus = .confirmed(metadata: sut.photoAttachment!)
         sut.description = "Test description"
+=======
+        sut.photo = UIImage() // Create dummy image
+        sut.disappearanceDate = Date()
+        sut.animalSpecies = .dog
+        sut.animalRace = "Labrador"
+        sut.animalGender = .male
+        sut.animalAge = 5
+        sut.animalLatitude = 52.2297
+        sut.animalLongitude = 21.0122
+        sut.animalAdditionalDescription = "Test description"
+>>>>>>> d4eaf68 (Fix ReportMissingPetFlowStateTests after Animal Description refactoring)
         sut.contactEmail = "test@example.com"
         sut.contactPhone = "123456789"
         
@@ -60,9 +84,21 @@ final class ReportMissingPetFlowStateTests: XCTestCase {
         
         // Then: All properties should be nil
         XCTAssertNil(sut.chipNumber)
+<<<<<<< HEAD
         XCTAssertNil(sut.photoAttachment)
         XCTAssertEqual(sut.photoStatus, .empty)
         XCTAssertNil(sut.description)
+=======
+        XCTAssertNil(sut.photo)
+        XCTAssertNil(sut.disappearanceDate)
+        XCTAssertNil(sut.animalSpecies)
+        XCTAssertNil(sut.animalRace)
+        XCTAssertNil(sut.animalGender)
+        XCTAssertNil(sut.animalAge)
+        XCTAssertNil(sut.animalLatitude)
+        XCTAssertNil(sut.animalLongitude)
+        XCTAssertNil(sut.animalAdditionalDescription)
+>>>>>>> d4eaf68 (Fix ReportMissingPetFlowStateTests after Animal Description refactoring)
         XCTAssertNil(sut.contactEmail)
         XCTAssertNil(sut.contactPhone)
         XCTAssertEqual(cache.clearCallCount, 1)
@@ -76,9 +112,21 @@ final class ReportMissingPetFlowStateTests: XCTestCase {
         
         // Then: All properties should remain nil
         XCTAssertNil(sut.chipNumber)
+<<<<<<< HEAD
         XCTAssertNil(sut.photoAttachment)
         XCTAssertEqual(sut.photoStatus, .empty)
         XCTAssertNil(sut.description)
+=======
+        XCTAssertNil(sut.photo)
+        XCTAssertNil(sut.disappearanceDate)
+        XCTAssertNil(sut.animalSpecies)
+        XCTAssertNil(sut.animalRace)
+        XCTAssertNil(sut.animalGender)
+        XCTAssertNil(sut.animalAge)
+        XCTAssertNil(sut.animalLatitude)
+        XCTAssertNil(sut.animalLongitude)
+        XCTAssertNil(sut.animalAdditionalDescription)
+>>>>>>> d4eaf68 (Fix ReportMissingPetFlowStateTests after Animal Description refactoring)
         XCTAssertNil(sut.contactEmail)
         XCTAssertNil(sut.contactPhone)
         XCTAssertEqual(cache.clearCallCount, 1)
